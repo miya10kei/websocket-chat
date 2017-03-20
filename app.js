e99fb77114e62e27c5cf3c7bf74bb0a3a708df13
miya10kei <miya10kei@gmail.com>
2017-03-21 01:54:11 +0900

    add exit button and process.

--- app.js
+++ app.js
@@ -9,13 +9,27 @@ var server = http.createServer(function(req, res) {
 var io = socketio.listen(server);
 
 io.sockets.on('connection', function(socket) {
-    var name;
+    var name = '';
+    var room = '';
+    socket.on('client_to_server_join', function(data) {
+        room = data.value;
+        socket.join(room);
+    });
+
+    socket.on('client_to_server_leave', function() {
+        var endMessage = name + "さんが退出しました。";
+        io.to(room).emit('server_to_client', {value: endMessage});
+        socket.leave(room);
+    });
+
     socket.on('client_to_server', function(data) {
-        io.sockets.emit('server_to_client', {value : data.value});
+        // io.sockets.emit('server_to_client', {value : data.value});
+        io.to(room).emit('server_to_client', {value: data.value});
     });
 
     socket.on('client_to_server_broadcast', function(data) {
-        socket.broadcast.emit('server_to_client', {value: data.value});
+        // socket.broadcast.emit('server_to_client', {value: data.value});
+        socket.broadcast.to(room).emit('server_to_client', {value: data.value});
     });
 
     socket.on('client_to_server_unicast', function(data) {
@@ -26,11 +40,12 @@ io.sockets.on('connection', function(socket) {
     });
 
     socket.on('disconnect', function() {
-        if (name == 'undefined') {
+        if (name == '') {
             console.log('未入室のままどこかに去っていきました。');
         } else {
             var endMessage = name + "さんが退出しました。";
-            io.sockets.emit('server_to_client', {value: endMessage});
+            // io.sockets.emit('server_to_client', {value: endMessage});
+            io.to(room).emit('server_to_client', {value: endMessage});
         }
     });
 });
